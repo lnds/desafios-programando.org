23fb8dfca310a6c230a91fd6df0d498ca827d19f
Eduardo Díaz <ediaz@dcc8090.cl>
2020-02-09 19:27:10 -0300

    solucion correcta a contando capicuas

--- 2020-02-09/contando-capicuas/src/main.rs
+++ 2020-02-09/contando-capicuas/src/main.rs
@@ -1,37 +1,35 @@
-use std::num::Wrapping;
+type Int = u64;
 
 fn main() {
     if let Some((num, c, n)) = (100..=1000)
-        .flat_map(|i| encontrar_capicua(Wrapping(i as u64), 0))
+        .flat_map(|i| encontrar_capicua(i, 1))
         .max_by(|(_, _, a), (_, _, b)| a.cmp(b))
     {
         println!("num = {}, su capicua es {}, iteraciones: {}", num, c, n);
     }
 }
 
-const LIMIT: u64 = 1_000_000;
+const LIMIT: Int = 1_000_000;
 
-fn encontrar_capicua(num: Wrapping<u64>, n: u64) -> Option<(Wrapping<u64>, Wrapping<u64>, u64)> {
-    let rev = reverse(num);
+fn encontrar_capicua(num: Int, n: Int) -> Option<(Int, Int, Int)> {
+    let rev = reverse(num)?;
     let sum = num + rev;
     if n > LIMIT {
         None
-    } else if sum == reverse(sum) {
-        Some((num, sum, n + 1))
+    } else if sum == reverse(sum)? {
+        Some((num, sum, n))
     } else {
-        encontrar_capicua(sum, n + 1).map(|(_, sum, n)| (num, sum, n + 1))
+        encontrar_capicua(sum, n + 1).map(|(_, sum, n)| (num, sum, n    ))
     }
 }
 
-fn reverse(num: Wrapping<u64>) -> Wrapping<u64> {
-    fn calc_capicua(num: Wrapping<u64>, rev: Wrapping<u64>) -> Wrapping<u64> {
-        let zero = Wrapping(0u64);
-        if num == zero {
-            rev
+fn reverse(num: Int) -> Option<Int> {
+    fn calc_capicua(num: Int, rev: Int) -> Option<Int> {
+        if num == 0 {
+            Some(rev)
         } else {
-            let ten = Wrapping(10u64);
-            calc_capicua(num / ten, ten * rev + num % ten)
+            calc_capicua(num.checked_div(10)?, (rev.checked_mul(10)?).checked_add(num.checked_rem(10)?)?)
         }
     }
-    calc_capicua(num, Wrapping(0u64))
+    calc_capicua(num, 0)
 }
