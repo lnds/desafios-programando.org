0c46d815675c46f25d28cf9c1a14c9cedd45572e
Eduardo Díaz <ediaz@dcc8090.cl>
2020-08-01 11:03:32 -0400

    agrega paginas html y error 404

--- 2020-07-31/a-rust-http-server/src/main.rs
+++ 2020-07-31/a-rust-http-server/src/main.rs
@@ -1,6 +1,6 @@
-use std::io;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::{fs, io};
 
 fn main() -> io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8088")?;
@@ -15,8 +15,20 @@ fn handle_connection(mut stream: TcpStream) -> io::Result<()> {
 
     stream.read(&mut buffer)?;
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    const GET: &[u8; 16] = b"GET / HTTP/1.1\r\n";
+    let (status, filename) = if buffer.starts_with(GET) {
+        ("HTTP/1.1 200 OK\r\n", "hola.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n", "404.html")
+    };
 
+    let content = fs::read_to_string(filename)?;
+    let response = format!(
+        "{}Content-Length: {}\r\n\r\n{}",
+        status,
+        content.len(),
+        content
+    );
     stream.write(response.as_bytes())?;
     stream.flush()
 }
